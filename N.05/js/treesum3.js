90294109e794b1d490efbb14288a205bc863b3f1
evgen <bashkoen@gmail.com>
2018-05-27 14:24:22 +0300

    добавил ==

--- N.05/js/treesum3.js
+++ N.05/js/treesum3.js
@@ -3,7 +3,7 @@ var massif = [5, 7, [4, [2], 8, [1, 3], 2], [9, [] ], 1, 8];
     var sum = 0;
     for (var i = 0; i < a.length; i++) {
       var v = a[i];
-      if (typeof v = 'object') {
+      if (typeof v == 'object') {
         s += treeSum(v)
       }
         else (s += v);
