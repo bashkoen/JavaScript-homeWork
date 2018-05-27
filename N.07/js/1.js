06df7f083d26934dc01deff9ac3e696a15e5aaaa
evgen <bashkoen@gmail.com>
2018-05-27 22:15:21 +0300

    добавка

--- N.07/js/1.js
+++ N.07/js/1.js
@@ -1,6 +1,6 @@
 var words = prompt("Введите слово");
 function wordVerification(words) {
     var enterWords = words.match(/[ёуеыаоэяию]/igm).length;
-    console.log(enterWords);
+    console.log("Количество букв в \"" + words + "\" " + enterWords);
 }
 wordVerification(words);
