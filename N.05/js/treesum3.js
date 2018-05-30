d8d506f270c78d81f28028293c53557ab8face97
evgen <bashkoen@gmail.com>
2018-05-30 18:01:34 +0300

    treeSum3 - не выводит сумму

--- N.05/js/treesum3.js
+++ N.05/js/treesum3.js
@@ -1,24 +1,18 @@
 var a = [5, 7, [4, [2], 8, [1, 3], 2], [9, [] ], 1, 8];
-  function treeSum(a) {
-    var sum = 0;
-    for (var i = 0; i < a.length; i++) {
+function treeSum(a) {
+    var s = 0;
+    for (var i=0; i < a.length; i++) {
       var v = a[i];
-      console.log(typeof v);
-      sum += v;
-      if (typeof v == "object")
-        sum += treeSum(v);
-      else 
-      sum += v;
+      s = s + v;
+      if (typeof v == 'object') {
+       s = s + treeSum(v)
+      }
+      else {
+      s = s + v
+      }
     }
-    return sum;
+    return s;
   }
-  treeSum(a);
-  console.log(typeof v);
-  console.log(treeSum(a));
-  //  massif.forEach(function(item, i, massif) {
-  //    Array.isArray(massif) ? treeSum(x) : sum += x;
-//    });
-//  return sum;
-//  alert(treeSum());
-//  }
-//treeSum();
+console.log(treeSum(a));
+
+ 
