2a68f70082386a68c4bac06bdc78785f560d9817
evgen <bashkoen@gmail.com>
2018-12-29 13:23:05 +0300

    29-12-2018

---
+++ shablon JS.1 задачи/js/1.js
@@ -0,0 +1,10 @@
+//получение данных id
+function over2() {
+    var but = document.getElementById("over2");
+    but.disabled = true;
+    //but.style = "color: red";
+}
+function over3() {
+    var but = document.getElementById("over2");
+    but.disabled = false;
+}
